5787af09910df17e7762fbcacb013dabe2bbb70f
Ninel0204 <116366686+Ninel0204@users.noreply.github.com>
2022-12-27 15:33:06 +0300

    Отформатирован код

--- src/test/groovy/ru/netology/MessageSenderImplTest.java
+++ src/test/groovy/ru/netology/MessageSenderImplTest.java
@@ -16,36 +16,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-
-
 public class MessageSenderImplTest {
     Map<String, String> headers = new HashMap<>();
     MessageSender messageSender;
     String expectedText;
 
-GeoService geoService= Mockito.mock(GeoService.class);
-LocalizationService localizationService=Mockito.mock(LocalizationService.class);
+    GeoService geoService = Mockito.mock(GeoService.class);
+    LocalizationService localizationService = Mockito.mock(LocalizationService.class);
 
 
-@BeforeEach
-    void BeforeEach(){
-    messageSender= new MessageSenderImpl(geoService,localizationService);
-    headers = new HashMap<>();
-}
+    @BeforeEach
+    void BeforeEach() {
+        messageSender = new MessageSenderImpl(geoService, localizationService);
+        headers = new HashMap<>();
+    }
 
-@Test
-    public void test_ru(){
-    headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.MOSCOW_IP);
-    expectedText = "Добро пожаловать";
-    Mockito.when(geoService.byIp(GeoServiceImpl.MOSCOW_IP))
-            .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
-    Mockito.when(localizationService.locale(Country.RUSSIA))
-            .thenReturn("Добро пожаловать");
-
-    Assertions.assertEquals(expectedText, messageSender.send(headers));
-}
     @Test
-    public void test_eng(){
+    public void test_ru() {
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.MOSCOW_IP);
+        expectedText = "Добро пожаловать";
+        Mockito.when(geoService.byIp(GeoServiceImpl.MOSCOW_IP))
+                .thenReturn(new Location("Moscow", Country.RUSSIA, null, 0));
+        Mockito.when(localizationService.locale(Country.RUSSIA))
+                .thenReturn("Добро пожаловать");
+
+        Assertions.assertEquals(expectedText, messageSender.send(headers));
+    }
+
+    @Test
+    public void test_eng() {
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, GeoServiceImpl.NEW_YORK_IP);
         expectedText = "Welcome";
         Mockito.when(geoService.byIp(GeoServiceImpl.NEW_YORK_IP))
